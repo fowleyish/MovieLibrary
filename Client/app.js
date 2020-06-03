c9cb86bd345621989a7e3ae7d38b62b07584b0f4
Fowley <alexanderfowle@gmail.com>
2020-06-03 14:48:20 -0500

    Testing with updateMovie()

--- Client/app.js
+++ Client/app.js
@@ -35,7 +35,7 @@
             success: function( data ){
                 $('#response pre').html('<hr /><table id="returnTable"><tr id="headingRow"><th>Title</th><th>Genre</th><th>Director</th><th></th><th></th></table>');
                 $.each(data, function( index ) {
-                    $('#returnTable').append( `<tr><td>${data[index].title}</td><td>${data[index].genre}</td><td>${data[index].director}</td><td><button class="updateBtn" onclick="invokeUpdateForm(${data[index].movieId})">Update</button></td><td><button class="deleteBtn" value="${data[index].movieId}">Delete</button></td></tr>` );
+                    $('#returnTable').append( `<tr><td>${data[index].title}</td><td>${data[index].genre}</td><td>${data[index].director}</td><td><button class="updateBtn" value="${data[index].movieId}">Update</button></td><td><button class="deleteBtn" value="${data[index].movieId}">Delete</button></td></tr>` );
                 })
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -53,9 +53,14 @@
             type: 'delete',
             contentType: 'application/json'
         });
+        getAllValues();
     }
 
-    $('.deleteBtn').click(console.log(this.value));
+    $('#response pre').on('click', '.updateBtn', this.value, function updateMovie(id, title, genre, director) {
+        $('#editForm').html('');
+        $('#editForm').html(`<h3>Update ${title}</h3><form id="editFormFields"><section id="movieTitleSectionEdit" class="formSection"> <label for="movieTitleInputEdit">Title</label> <input type="text" name="movieTitleEdit" id="movieTitleInputEdit" value="${title}" placeholder="Enter a movie title"/> </section> <section id="movieGenreSectionEdit" class="formSection"> <label for="movieGenreInputEdit">Genre</label> <input type="text" name="movieGenreEdit" id="movieGenreInputEdit" value="${genre}" placeholder="Enter a genre"/> </section> <section id="movieDirectorSectionEdit" class="formSection"> <label for="movieDirectorInputEdit">Director</label> <input type="text" name="movieDirectorEdit" id="movieDirectorInputEdit" value="${director}" placeholder="Enter a director's name"/> </section> <section id="submitButtonsSectionEdit" class="formSection"> <input type="submit" id="submitEdit" value="Update"/> </section></form>`)
+    });
+    
     $('#moviesForm').submit( processFormPost );
     $('#submitGetAll').click( getAllValues );
 })(jQuery);
