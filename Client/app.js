639c99a1a56b3f9bf895f94c7404d8c870e851ee
Fowley <alexanderfowle@gmail.com>
2020-06-03 16:50:10 -0500

    Updates to edit/put functions

--- Client/app.js
+++ Client/app.js
@@ -56,16 +56,59 @@
         getAllValues();
     }
 
+    function updateMovie(data) {
+        var dict = {
+        	Title : data.title,
+            Director: data.director,
+            Genre: data.genre,
+            Id: data.movieId
+        };
 
-
-
-
-
-
-    $('#response pre').on('click', '.updateBtn', this.value, function updateMovie(id, title, genre, director) {
         $('#editForm').html('');
-        $('#editForm').html(`<h3>Update ${title}</h3><form id="editFormFields"><section id="movieTitleSectionEdit" class="formSection"> <label for="movieTitleInputEdit">Title</label> <input type="text" name="movieTitleEdit" id="movieTitleInputEdit" value="${title}" placeholder="Enter a movie title"/> </section> <section id="movieGenreSectionEdit" class="formSection"> <label for="movieGenreInputEdit">Genre</label> <input type="text" name="movieGenreEdit" id="movieGenreInputEdit" value="${genre}" placeholder="Enter a genre"/> </section> <section id="movieDirectorSectionEdit" class="formSection"> <label for="movieDirectorInputEdit">Director</label> <input type="text" name="movieDirectorEdit" id="movieDirectorInputEdit" value="${director}" placeholder="Enter a director's name"/> </section> <section id="submitButtonsSectionEdit" class="formSection"> <input type="submit" id="submitEdit" value="Update"/> </section></form>`)
+        $('#editForm').html(`<h3>Update ${dict.Title}</h3><form id="editFormFields"><section id="movieTitleSectionEdit" class="formSection"> <label for="movieTitleInputEdit">Title</label> <input type="text" name="movieTitleEdit" id="movieTitleInputEdit" value="${dict.Title}" placeholder="Enter a movie title"/> </section> <section id="movieGenreSectionEdit" class="formSection"> <label for="movieGenreInputEdit">Genre</label> <input type="text" name="movieGenreEdit" id="movieGenreInputEdit" value="${dict.Genre}" placeholder="Enter a genre"/> </section> <section id="movieDirectorSectionEdit" class="formSection"> <label for="movieDirectorInputEdit">Director</label> <input type="text" name="movieDirectorEdit" id="movieDirectorInputEdit" value="${dict.Director}" placeholder="Enter a director's name"/> </section> <section id="submitButtonsSectionEdit" class="formSection"> <input type="submit" id="submitEdit" value="Update"/> </section></form>`)
+    }
+
+    $('#response pre').on('click', '.updateBtn', this.value, function getSingleMovieForUpdate( e ) {
+        $.ajax({
+            url: 'https://localhost:44325/api/movie/' + this.value,
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            success: function(data) {
+                updateMovie(data);
+            }
+        })
     });
+
+
+    // Not working completely; need to find a way to include the ID of the movie so that we can supply that to the controller
+    $('#editForm').on('click', '#submitEdit', function updateMoviePut( e ) {
+        var dict = {
+        	Title : $('#movieTitleInputEdit').val(),
+            Director: $('#movieDirectorInputEdit').val(),
+            Genre: $('#movieGenreInputEdit').val(),
+            Id: this.movieId
+        };
+
+        if (dict.Title != '') {
+            $.ajax({
+                url: 'https://localhost:44325/api/movie/' + id,
+                dataType: 'json',
+                type: 'put',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function( data ){
+                    $('#editForm').html('');
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+        }
+
+        e.preventDefault();
+    });
+
 
     // $('#response pre').on('click', '.deleteBtn', deleteMovie(this.value));
     // $('.deleteBtn').click(console.log(this.value));
